1378e16fd0c561e56d8a71fe38cfac52f8124c50
Rafael Malave <rafomalave@gmail.com>
2018-01-25 20:13:21 -0800

    styles for howto

--- scripts/views/how-to-view.js
+++ scripts/views/how-to-view.js
@@ -6,6 +6,8 @@ if (!localStorage.getItem('beenHere')){
      $('#intro').hide();
      console.log('been here is here')
  }
+
+ $(".ui-dialog-titlebar").hide()
     
       
  $('#intro').on('click','button',function(e){
@@ -19,14 +21,11 @@ if (!localStorage.getItem('beenHere')){
       $('.page').hide()
       $('#intro').hide()
       $('#howThisWorks').show()
-      $('#howto-page').html(`<h1>What Is Crypto Currency</h1>
-<p>To better understand crypto currency, here are a couple videos to better understand things. 
-   <p><iframe width="560" height="315" src="https://www.youtube.com/embed/KP_hGPQVLpA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></p>
-      <p><iframe width="560" height="315" src="https://www.youtube.com/embed/kubGCSj5y3k" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></p>
-    <h1>How to Buy Cryptocurrency</h1>
-      <p>There are many diffrent formats available to buy and sell crypto currency, but here is one way. You can start by making an account on <a href="http://www.coinbase.com"> Coinbase</a>. Coinbase will allow you to buy and trade the better known cryptocurrencies like, Bitcoin, Bitcoin Cash, Ethereum, and Litecoin. You can link a credit/debit card for immediate trading or you can use a direct link to your bank account and wait four to five days. To trade some of the other cryptocurrencies like Ripple, Tron, and Cardano other sites are available like <a href="http://www.binance.com"> Binance</a>.</p>`)
       $('#howto-page').show()
     }
+
+        // Target your .container, .wrapper, .post, etc.
+    $("#howto-page").fitVids();
  })
  
  $('.page').hide()
